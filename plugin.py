79ff6d5113c2c5c424381ec956aa323d4f388bae
Markus Rempfler <markus.rempfler@fmi.ch>
2019-09-23 14:39:30 +0200

    Add dialog for credentials on activation

--- plugin.py
+++ plugin.py
@@ -9,6 +9,9 @@ import logging
 import configparser
 import pyodbc
 
+from tkinter import Label, Entry, Toplevel
+import tkinter.simpledialog
+
 # NOTE try the relative import in case mldpy is also placed in the
 # plugin folder of FAIM-Robocopy.
 try:
@@ -32,6 +35,49 @@ CONFIG_FNAME = os.path.join(os.path.dirname(__file__), 'mdcstore_updater.ini')
 CONFIG = get_config(CONFIG_FNAME)
 
 
+class CredentialsDialog(tkinter.simpledialog.Dialog):
+    def __init__(self,
+                 title,
+                 initial_user=None,
+                 initial_password=None,
+                 parent=None):
+        '''
+        '''
+        # NOTE default parent as in tkinter.simpledialog._QueryDialog
+        if parent is None:
+            parent = tkinter._default_root
+        self.initial_user = initial_user
+        self.initial_password = initial_password
+
+        tkinter.simpledialog.Dialog.__init__(self, parent, title)
+
+    def body(self, master):
+        '''
+        '''
+        Label(master, text='Username').grid(row=0)
+        Label(master, text='Password').grid(row=1)
+
+        self.username_field = Entry(master)
+        self.password_field = Entry(master, show='*')
+
+        if self.initial_user is not None:
+            self.username_field.insert(0, self.initial_user)
+        if self.initial_password is not None:
+            self.password_field.insert(0, self.initial_password)
+
+        self.username_field.grid(row=0, column=1)
+        self.password_field.grid(row=1, column=1)
+
+        return self.username_field  # initial focus
+
+    def apply(self):
+        '''
+        '''
+        self.result = {}
+        self.result['username'] = self.username_field.get()
+        self.result['password'] = self.password_field.get()
+
+
 class MDCStoreUpdaterPlugin:
     '''
     '''
@@ -53,10 +99,24 @@ class MDCStoreUpdaterPlugin:
         '''
         self.logger.info('MDCStore Updater activated. Testing connection...')
         try:
+            self.ask_for_credentials()
             with MDCStoreHandle(**CONFIG):
                 self.logger.info('MDCStore connection successful.')
+            return True
+
         except pyodbc.Error as err:
             self.logger.error('Connection could not be established: %s', err)
+            return False
+
+    def ask_for_credentials(self):
+        '''
+        '''
+        credentials = CredentialsDialog(
+            title='MDCStore Credentials',
+            initial_user=CONFIG.get('username', None),
+            initial_password=CONFIG.get('password', None)).result
+        if credentials is not None:
+            CONFIG.update(credentials)
 
     def on_call(self):
         '''
